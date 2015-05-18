a08e01dc4d6cfd54e487d313592ea302e4ec1b4e
Iyad Horani <i3d@ironic3d.com>
2015-05-18 14:45:15 +1000

    Initial Commit

---
+++ XBlaster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XBlaster
+//
+//  Created by Iyad Horani on 18/05/2015.
+//  Copyright (c) 2015 IRONIC3D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
